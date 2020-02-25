d369e45a9695893f502340c7840dab720fc1ce66
kelian <kelianferhat@gmail.com>
2020-02-25 11:53:09 +0900

    Added Class

--- GreyBox/Backtest/Backtesting.py
+++ GreyBox/Backtest/Backtesting.py
@@ -11,14 +11,16 @@ import pandas as pd
 from xbbg import blp
 
 
+class TimeSeries(self, Liste):
+    def __init__(self):
+        return 1
 
-
-def GetRet(Liste, Start = "20150101"):
-    Start = "20150101"
-    End = pd.Timestamp.today().strftime("%Y%m%d")
-
-    Res = blp.bdh(tickers= [x + " Equity" for x in Liste],start_date = Start , end_date = End)
-    Res.columns = [x[0] for x in Res.columns]
-    Ret = (np.log(Res) - np.log(Res.shift(1))).iloc[1:,:]
-    Ret = Ret.T.mean()
-    return Ret
+    def GetRet(Liste, Start = "20150101"):
+        Start = "20150101"
+        End = pd.Timestamp.today().strftime("%Y%m%d")
+    
+        Res = blp.bdh(tickers= [x + " Equity" for x in Liste],start_date = Start , end_date = End)
+        Res.columns = [x[0] for x in Res.columns]
+        Ret = (np.log(Res) - np.log(Res.shift(1))).iloc[1:,:]
+        Ret = Ret.T.mean()
+        return Ret
--- conda_package/GreyBox/bld.bat
+++ conda_package/GreyBox/bld.bat
@@ -1,5 +1,1 @@
-if errorlevel 1 exit 1
-
-"%PYTHON%" setup.py install --single-version-externally-managed --record=record.txt
-
-if errorlevel 1 exit 1
+"%PYTHON%" import numpy as np
